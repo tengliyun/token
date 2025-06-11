479db58ceff210db2229106c0de38b7415613a6f
jundayw <jundayw@126.com>
2025-06-11 20:48:00 +0800

    wip:config

--- config/token.php
+++ config/token.php
@@ -1,5 +1,7 @@
 <?php
 
+use Token\JWT\Signature\Rsa\RS512;
+
 return [
 
     /*
@@ -17,18 +19,47 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Token Signer Class
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the signer class used for signing and verifying JWTs.
+    |
+    | Supported: ES256::class, ES384::class, ES512::class,
+    |            RS256::class, RS384::class, RS512::class,
+    |            HS256::class, HS384::class, HS512::class
+    |
+    */
+
+    'signer' => RS512::class,
+
+    /*
+    |--------------------------------------------------------------------------
     | Encryption Keys
     |--------------------------------------------------------------------------
     |
-    | Token uses encryption keys while generating secure access tokens for
-    | your application. By default, the keys are stored as local files but
-    | can be set via environment variables when that is more convenient.
+    | These keys are used for signing and verifying tokens when using
+    | asymmetric algorithms such as RS256, RS512, or ES512. By default,
+    | the keys are stored as local files, but you may also load them
+    | from environment variables if that is more convenient.
     |
     */
 
     'private_key' => env('TOKEN_PRIVATE_KEY', 'token_private_key.pem'),
 
     'public_key' => env('TOKEN_PUBLIC_KEY', 'token_public_key.pem'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Secret Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is used for signing tokens when using symmetric algorithms
+    | such as HMAC (e.g., HS256, HS512). It must be provided as plain text
+    | and can be set via environment variables for convenience.
+    |
+    */
+
+    'secret_key' => env('TOKEN_SECRET_KEY', env('APP_KEY')),
 
     /*
     |--------------------------------------------------------------------------
