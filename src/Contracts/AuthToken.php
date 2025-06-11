4935c4bd36c1360b65043a2f94a5435a3d77d2d4
jundayw <jundayw@126.com>
2025-06-11 20:48:17 +0800

    AuthToken

--- src/Contracts/AuthToken.php
+++ src/Contracts/AuthToken.php
@@ -2,7 +2,15 @@
 
 namespace Tengliyun\Token\Contracts;
 
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
 interface AuthToken
 {
+    public function tokenable(): MorphTo;
 
+    public function findToken(string $token);
+
+    public function can(string $ability): bool;
+
+    public function cant(string $ability): bool;
 }
