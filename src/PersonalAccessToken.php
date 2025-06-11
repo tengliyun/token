a62c28f0f24374e81b9bccc6cc0f2d0c6c19de9b
jundayw <jundayw@126.com>
2025-06-11 20:47:18 +0800

    PersonalAccessToken

---
+++ src/PersonalAccessToken.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tengliyun\Token;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Tengliyun\Token\Contracts\AuthToken;
+use Token\JWT\Contracts\Token as JWToken;
+use Token\JWT\Factory;
+
+class PersonalAccessToken implements Arrayable
+{
+    protected ?JWToken $accessToken  = null;
+    protected ?JWToken $refreshToken = null;
+
+    public function __construct(protected Factory $factory)
+    {
+        //
+    }
+
+    public function accessToken(AuthToken $model): static
+    {
+        $this->accessToken = $this->factory->builder()
+            // Configures the issuer (iss claim)
+            ->issuedBy($model->getAttribute('tokenable_type'))
+            // Configures the id (jti claim)
+            ->identifiedBy($model->getKey())
+            // Configures the subject
+            ->relatedTo('access-token')
+            // Configures the audience (aud claim)
+            ->permittedFor(...$model->getAttribute('scopes'))
+            // Configures the time that the token was issue (iat claim)
+            ->issuedAt(now()->toDateTimeImmutable())
+            // Configures the time that the token can be used (nbf claim)
+            ->canOnlyBeUsedAfter(now()->toDateTimeImmutable())
+            // Configures the expiration time of the token (exp claim)
+            ->expiresAt($model->getAttribute('access_token_expire_at'))
+            // Builds a new token
+            ->getToken($this->factory->signer(), $this->factory->signingKey());
+
+        return $this;
+    }
+
+    public function refreshToken(AuthToken $model): static
+    {
+        $this->refreshToken = $this->factory->builder()
+            // Configures the issuer (iss claim)
+            ->issuedBy($model->getAttribute('tokenable_type'))
+            // Configures the id (jti claim)
+            ->identifiedBy($model->getKey())
+            // Configures the subject
+            ->relatedTo('refresh-token')
+            // Configures the time that the token was issue (iat claim)
+            ->issuedAt(now()->toDateTimeImmutable())
+            // Configures the time that the token can be used (nbf claim)
+            ->canOnlyBeUsedAfter($model->getAttribute('access_token_expire_at'))
+            // Configures the expiration time of the token (exp claim)
+            ->expiresAt($model->getAttribute('refresh_token_expire_at'))
+            // Builds a new token
+            ->getToken($this->factory->signer(), $this->factory->signingKey());
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     * @return array
+     */
+    #[\Override]
+    public function toArray(): array
+    {
+        return array_filter([
+            'access_token'  => $this->accessToken ? Token::useEncryptTokens($this->accessToken->toString()) : null,
+            'refresh_token' => $this->refreshToken ? Token::useEncryptTokens($this->refreshToken->toString()) : null,
+        ]);
+    }
+}
