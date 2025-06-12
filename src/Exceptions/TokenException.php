6230244b04c62c01c289218788b403b15291e152
jundayw <jundayw@126.com>
2025-06-12 17:16:36 +0800

    chore:TokenException

--- src/Exceptions/TokenException.php
+++ src/Exceptions/TokenException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class TokenException extends Exception
 {
-    public function __construct(string $message = '', int $code = 40001, Throwable $previous = null)
+    public function __construct(string $message = 'TokenException', int $code = 40001, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
