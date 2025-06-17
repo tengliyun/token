d72183bfd94557341efb9c837d1058c2730ccd20
jundayw <jundayw@126.com>
2025-06-17 15:35:19 +0800

    TokenAuthenticated

--- src/Events/TokenAuthenticated.php
+++ src/Events/TokenAuthenticated.php
@@ -2,11 +2,14 @@
 
 namespace Tengliyun\Token\Events;
 
+use Illuminate\Queue\SerializesModels;
 use Tengliyun\Token\Contracts\AuthToken;
 use Tengliyun\Token\Contracts\HasApiToken;
 
 class TokenAuthenticated
 {
+    use SerializesModels;
+
     public function __construct(
         public HasApiToken $tokenable,
         public AuthToken   $token,
