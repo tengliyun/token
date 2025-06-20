8893669d87db883c1b00abe757ff482995e6c9aa
jundayw <jundayw@126.com>
2025-06-20 14:26:14 +0800

    TokenUserProvider

---
+++ src/Auth/TokenUserProvider.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tengliyun\Token\Auth;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+
+class TokenUserProvider implements UserProvider
+{
+    /**
+     * Create a new passport user provider.
+     *
+     * @param UserProvider $provider
+     * @param string|null  $providerName
+     */
+    public function __construct(
+        protected UserProvider $provider,
+        protected ?string      $providerName,
+    )
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return Authenticatable|null
+     */
+    public function retrieveById(mixed $identifier): ?Authenticatable
+    {
+        return $this->provider->retrieveById($identifier);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return Authenticatable|null
+     */
+    public function retrieveByToken(mixed $identifier, $token): ?Authenticatable
+    {
+        return $this->provider->retrieveByToken($identifier, $token);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return void
+     */
+    public function updateRememberToken(Authenticatable $user, $token): void
+    {
+        $this->provider->updateRememberToken($user, $token);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials): ?Authenticatable
+    {
+        return $this->provider->retrieveByCredentials($credentials);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return bool
+     */
+    public function validateCredentials(Authenticatable $user, array $credentials): bool
+    {
+        return $this->provider->validateCredentials($user, $credentials);
+    }
+
+    /**
+     * Get the name of the user provider.
+     *
+     * @return string|null
+     */
+    public function getProviderName(): ?string
+    {
+        return $this->providerName;
+    }
+}
